949aebef52a61515671446c20ba389305501a0b0
lingshuiyoulan@163.com <lanling0118>
2017-10-28 11:12:34 +0800

    update

--- app/src/main/java/com/lanling/sendmessage/email/SendMsgUtil.java
+++ app/src/main/java/com/lanling/sendmessage/email/SendMsgUtil.java
@@ -1,42 +1,20 @@
 package com.lanling.sendmessage.email;
 
-import android.util.Log;
-
 /**
- * @author lanling
- *         on 2017/8/6.
+ * The Class
+ *
+ * @author Lanling
+ *         on 2017/10/28
  */
-
 public class SendMsgUtil {
-    public static final String TAG = "MsgUtil";
-
-    public static void sendMessage(final String msg) {
-        Log.i(TAG, "开始发送邮件");
-        // 这个类主要是设置邮件
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                MailSenderInfo mailInfo = new MailSenderInfo();
-                mailInfo.setMailServerHost("smtp.163.com");
-                mailInfo.setMailServerPort("25");
-                mailInfo.setValidate(true);
-                mailInfo.setUserName("17797754554@163.com");
-                mailInfo.setPassword("ruoshui0118");// 您的邮箱密码
-                mailInfo.setFromAddress("17797754554@163.com");
-                mailInfo.setToAddress("1530231611@qq.com");
-                mailInfo.setSubject("短信通知");
-                mailInfo.setContent(msg);
-                // 这个类主要来发送邮件
-                SimpleMailSender sms = new SimpleMailSender();
-                boolean isSuccess = false;
-                isSuccess = sms.sendTextMail(mailInfo);// 发送文体格式
-//                isSuccess = sms.sendHtmlMail(mailInfo);//发送html格式
-                if (isSuccess) {
-                    Log.i(TAG, "发送成功");
-                } else {
-                    Log.i(TAG, "发送失败");
-                }
-            }
-        }).start();
+    public static void sendMessage(String message) {
+        Email email = new Email("smtp.163.com");//这里以网易邮箱为例子
+        email.setNeedAuth(true);
+        email.setSubject("短信通知");//邮件主题
+        email.setBody(message);//邮件正文
+        email.setTo("1530231611@qq.com");//收件人地址
+        email.setFrom("17797754554");//发件人地址
+        email.setNamePass("lingshuiyoulan@163.com", "ruoshui0118");//发件人地址和密码 **改为相应邮箱密码
+        email.sendout();
     }
 }
--- app/src/main/java/com/lanling/sendmessage/service/MyService.java
+++ app/src/main/java/com/lanling/sendmessage/service/MyService.java
@@ -4,7 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
-
 import com.lanling.sendmessage.email.SendMsgUtil;
 
 /**
